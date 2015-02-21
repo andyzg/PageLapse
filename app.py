39d90d92b0205d5f190c2921de3021b637e2f90e
Andy Zhang <andzhng@gmail.com>
2015-02-21 12:11:10 -0500

    Added Jade as template engine

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/jade')
+def test_jade():
+    return render_template('test.jade')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
