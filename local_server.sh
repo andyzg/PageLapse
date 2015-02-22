74211f623179da1103cd6393f84e95a2b0534703
root <michaelhochum@gmail.com>
2015-02-22 12:36:44 -0500

    Fixed script

--- local_server.sh
+++ local_server.sh
@@ -1,7 +1,7 @@
 set -e
 
-kill $(ps aux | grep 'jekyll serve' | awk '{print $2}')
-kill $(ps aux | grep ‘phantomjs' | awk '{print $2}')
+kill $(ps aux | grep 'jekyll serve' | awk '{print $2}') || true
+kill $(ps aux | grep 'phantomjs' | awk '{print $2}') || true
 grunt lib
 grunt js
 grunt sass
